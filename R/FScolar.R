2457021953a78ecf7ba1bdb62f7ad59cbee106c3
fabiosalle <salles.fabio@gmail.com>
2020-04-27 22:02:21 -0300

    Initial commit

---
+++ R/FScolar.R
@@ -0,0 +1,14 @@
+#' Cola dois paramentros de entrada
+#'
+#' Recebe dois parametros e colam os mesmos numa unica saída
+#'
+#' @param a primeiro parametro
+#' @param b segundo parametro
+#'
+#' @return o paste dos dois parametros
+#'
+#' @export
+
+FScolar <-  function (a,b){
+  paste0(a,b)
+}
